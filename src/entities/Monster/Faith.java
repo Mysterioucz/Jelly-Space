43e4d6fbaeab85e313efc125f961523836dc7be5
Prime <chayaphon33793@gmail.com>
2024-05-14 06:35:12 +0700

    - balance

--- src/entities/Monster/Faith.java
+++ src/entities/Monster/Faith.java
@@ -10,7 +10,7 @@ public class Faith extends Base_Monster implements Attackable, Guardable, Unique
     private Image img = new Image(ClassLoader.getSystemResource("img/entities/monster/Faith/Idle.gif").toString());
 
     public Faith(int x, int y){
-        super("Faith", null,1000,1000,300,200,60,false,x,y,192,192,0,null);
+        super("Faith", null,1500,1000,300,200,60,false,x,y,192,192,0,null);
         setImage(img);
         setIdle_battle_img(getName());
         setSpecial_img(getName());
